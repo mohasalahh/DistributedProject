62c25e2c9fea80bb9d6d9d05b2aa0ee6bcb6f303
Mohamed Salah <me@mohasalah.com>
2024-05-16 20:02:41 +0300

    add basic flask setup

---
+++ src/web_server/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file:
+        file.save('/Users/mohamedsalah/Documents/Mixes/DistributedProject/uploaded_imgs/' + file.filename)
+        return 'File uploaded successfully'
+
+if __name__ == '__main__':
+    app.run(debug=True)
