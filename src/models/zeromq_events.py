6882688b0e60d0a0da9da8d28f284a557f816eb3
Mohamed Salah <me@mohasalah.com>
2024-05-16 22:05:27 +0300

    add some update messages to queue from worker

--- src/models/zeromq_events.py
+++ src/models/zeromq_events.py
@@ -6,5 +6,8 @@ class ZeroMQEvent(Enum):
     NODE_FAILED = 1
     PROCESSING_FAILED = 2
     PROCESSING_DONE = 3
+    '''
+    Fired when one node is done.
+    '''
     PROGRESS_UPDATE = 4
     START_PROCESSING = 5
--- src/mpi/worker.py
+++ src/mpi/worker.py
@@ -7,7 +7,9 @@ from mpi4py import MPI
 import threading
 
 from models.image_processing_task import ImageOperation, ImageProcessingTask
+from models.zeromq_events import ZeroMQEvent
 from mpi.processing import apply, divide_image_into_arrays
+from zmq_helpers.zmq_sender import send_to_zmq
 
 class WorkerThread(threading.Thread):
     """
@@ -29,26 +31,40 @@ class WorkerThread(threading.Thread):
         comm = MPI.COMM_WORLD
         rank = comm.Get_rank()
         size = comm.Get_size()
+        
+        try:
+            # Load image data on rank 0
+            if rank == 0:
+                image_data = divide_image_into_arrays(self.task.img_path, size)
+            else:
+                image_data = None
 
-        # Load image data on rank 0
-        if rank == 0:
-            image_data = divide_image_into_arrays(self.task.img_path, size)
-        else:
-            image_data = None
+            # Scatter image data to all processes
+            local_chunk = comm.scatter(image_data, root=0)
 
-        # Scatter image data to all processes
-        local_chunk = comm.scatter(image_data, root=0)
+            # Apply Gaussian filter to local chunk of image data
+            filtered_chunk = apply(self.task.operation_type, local_chunk)
 
-        # Apply Gaussian filter to local chunk of image data
-        filtered_chunk = apply(self.task.operation_type, local_chunk)
+            # Gather filtered chunks from all processes on rank 0
+            all_filtered_chunks = comm.gather(filtered_chunk, root=0)
 
-        # Gather filtered chunks from all processes on rank 0
-        all_filtered_chunks = comm.gather(filtered_chunk, root=0)
+            # On rank 0, combine filtered chunks and save the filtered image
+            if rank == 0:
+                filtered_image = np.concatenate(all_filtered_chunks, axis=1)
+                cv2.imwrite(self.task.get_save_path(), filtered_image) 
+                data = " ".join([self.task.id, self.task.get_save_path()])
+                send_to_zmq(ZeroMQEvent.PROCESSING_DONE, data)
+            else:
+                send_to_zmq(ZeroMQEvent.PROGRESS_UPDATE, self.task.id)
+        except Exception as e:
+            if rank == 0:
+                data = " ".join([self.task.id])
+                send_to_zmq(ZeroMQEvent.PROCESSING_FAILED, data)
+            else:
+                data = " ".join([self.task.id, rank])
+                send_to_zmq(ZeroMQEvent.NODE_FAILED, data)
 
-        # On rank 0, combine filtered chunks and save the filtered image
-        if rank == 0:
-            filtered_image = np.concatenate(all_filtered_chunks, axis=1)
-            cv2.imwrite(self.task.get_save_path(), filtered_image) 
+
 
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
